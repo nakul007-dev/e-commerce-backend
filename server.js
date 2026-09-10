74136a52f197a62b4d97b9c8ae8bf024e340bade
nakul007-dev <nakulsharma2k17@gmail.com>
2026-09-10 17:30:57 +0530

    backend server data

---
+++ server.js
@@ -0,0 +1,16 @@
+import jsonServer from 'json-server';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on ${port}`);
+});
