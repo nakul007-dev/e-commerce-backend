ab7f10f4c3dacbf190d7c0c2be10af3f7a1018c9
nakul007-dev <nakulsharma2k17@gmail.com>
2026-09-11 12:23:32 +0530

    chnages to deploy

--- api/index.js
+++ api/index.js
@@ -3,14 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
 const port = process.env.PORT || 3001;
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on ${port}`);
-});
